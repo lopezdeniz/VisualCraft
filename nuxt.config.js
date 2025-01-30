ad8583704522ed94dc080617e62f19592759ceb6
mrLopez <lopesdenis@mail.ru>
2025-01-29 22:01:47 -0500

    add google script

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,11 @@ export default defineNuxtConfig({
         {
           src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js', // Bootstrap JS через CDN
           type: 'text/javascript'
+        },
+        {
+          src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9881364321502260',
+          async: true,
+          crossorigin: 'anonymous'
         }
       ]
     }
